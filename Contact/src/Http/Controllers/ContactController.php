2aeb8091320d71371b77accbc20941db37400539
Mamad <mamadbosqests@gmail.com>
2021-11-05 18:30:09 +0330

    send the email to current user

--- Contact/src/Http/Controllers/ContactController.php
+++ Contact/src/Http/Controllers/ContactController.php
@@ -3,8 +3,10 @@
 namespace Contact\Form\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Contact\Form\Email\ContactMail;
 use Contact\Form\Models\Contact;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
 {
@@ -22,6 +24,8 @@ class ContactController extends Controller
        ]);
 
        Contact::create($data);
+
+       Mail::to("mamadch@gmail.com")->send(new ContactMail());
 
        return back();
 
